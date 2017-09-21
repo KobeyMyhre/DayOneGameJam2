c3ee1fb095fe02b20e9b69735c4465a1209702be
Kobey Myhre <s179139@students.aie.edu.au>
2017-09-21 15:45:15 -0700

    Nothing

--- SHMup/Assets/Spawner.cs
+++ SHMup/Assets/Spawner.cs
@@ -2,27 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour,Idamageable, Ikillable {
 
     public GameObject[] Enemies;
     public int[] count;
     public float timer;
     private float startTime;
-
+    public float health;
+    public int RANGE;
+    public int size;
 	// Use this for initialization
 	void Start () {
         startTime = timer;
+        timer = 0;
 	}
 	
+    public void Die()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void TakeDamage(float damageTaken)
+    {
+        health -= damageTaken;
+    }
+
 
     void SpawnEnemy()
     {
-        int idx = Random.Range(0,3);
+        int idx = Random.Range(0,size -1);
         var SpawnBaby = Enemies[idx];
         for(int i = 0; i < count[idx]; i++)
         {
-            int x = Random.Range(-10, 10);
-            int z = Random.Range(-10, 10);
+            int x = Random.Range(-RANGE, RANGE);
+            int z = Random.Range(-RANGE, RANGE);
             SpawnBaby.transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
             Instantiate(SpawnBaby);
         }
